4981b85ae6d9999ca710d94da6e5f20f44bb980e
jaynara95 <jay.95nascimento@gmail.com.br>
2023-01-21 17:59:52 -0300

    feat: conclusao do mobile e animacoes

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,25 @@ var swiper = new Swiper(".slide-character", {
     slidesPerView: 3.5,
     spaceBetween: 19,
     freeMode: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1.1,
+        spaceBetween: 12,
+      },
+      768: {
+        slidesPerView: 2.1,
+      },
+      991: {
+        slidesPerView: 2.8,
+      },
+      1200: {
+        slidesPerView: 3.5,
+        spaceBetween: 19,
+      },
+    }
+  });
+
+  AOS.init({
+    duration: 1500,
+    once: true,
   });
